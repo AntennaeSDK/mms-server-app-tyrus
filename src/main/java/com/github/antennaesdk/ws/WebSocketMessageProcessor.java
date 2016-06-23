5c0e25e527c4aa1a00313d943dc9192ab75f74ca
S Nambi <snambi@gmail.com>
2016-06-22 22:43:57 -0700

    done

--- src/main/java/com/github/antennaesdk/ws/WebSocketMessageProcessor.java
+++ src/main/java/com/github/antennaesdk/ws/WebSocketMessageProcessor.java
@@ -3,12 +3,15 @@
  */
 package com.github.antennaesdk.ws;
 
-import org.antennae.common.messages.Message;
+import org.antennae.common.messages.ClientMessage;
+import org.antennae.common.messages.ServerTrackedMessage;
 import org.glassfish.tyrus.client.ClientManager;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Calendar;
+import java.util.Date;
 import javax.websocket.*;
 
 @ClientEndpoint
@@ -24,14 +27,34 @@ public class WebSocketMessageProcessor {
     }
 
     public void processTextMessage( String m ){
+
         if( m != null ){
-            Message message = Message.fromJson(m);
+
+            ServerTrackedMessage message = ServerTrackedMessage.fromJson(m);
+
+            ClientMessage clientMessage = new ClientMessage();
+            clientMessage.setTo( message.getServerMessage().getFrom());
+
+            String payload = message.getServerMessage().getPayLoad();
+            long time = Calendar.getInstance().getTimeInMillis();
+            Date date = new Date(time);
+
+            String newPayload = payload + "; time =" + date.toString();
+            clientMessage.setPayLoad( newPayload );
+
+            message.setClientMessage(clientMessage);
+
+            if( session != null && session.isOpen() ){
+                session.getAsyncRemote().sendText( message.toJson() );
+            }
         }
     }
 
     @OnMessage
     public void onMessage(String message) {
         System.out.println("Received msg: "+message);
+
+        processTextMessage(message);
     }
 
     @OnOpen
